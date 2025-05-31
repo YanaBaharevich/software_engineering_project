1750e23541edf4b6bd361fee1609cb66f29dd851
YanaBaharevich <w68369@student.wsiz.edu.pl>
2025-05-31 02:03:11 +0200

    1

--- body.py
+++ body.py
@@ -1,1 +1,15 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
+from ttkthemes import ThemedTk
+
+default_bg = "#e4c0a8"
+app = ThemedTk(theme="radiance")
+main_frame = tk.Frame(app, bg=default_bg)
+
+app.configure(bg=default_bg)
+main_frame.pack(fill="both", expand=True)
+
+app.title("Notanik")
+app.geometry("700x500")
+
+app.mainloop()
