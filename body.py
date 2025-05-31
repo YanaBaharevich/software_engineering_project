43fe0733fc483bc0c3279048040e2ff2510066ff
YanaBaharevich <w68369@student.wsiz.edu.pl>
2025-05-31 02:45:11 +0200

    3

--- body.py
+++ body.py
@@ -8,6 +8,7 @@ app = tb.Window(themename="vapor")
 app.title("Notatnik")
 app.geometry("900x600")
 app.minsize(700, 500)
+app.style.configure('.', font=('Arial ', 13))
 
 app.rowconfigure(0, weight=0)     # Zona przycisków
 app.columnconfigure(0, weight=3)  # Lewa
@@ -15,7 +16,7 @@ app.columnconfigure(1, weight=1)  # Prawa
 app.rowconfigure(1, weight=0)
 app.rowconfigure(2, weight=1)     # Zona notatek
 
-top_frame = tk.Frame(app)
+top_frame = tb.Frame(app)
 top_frame.grid(row=0, column=0, columnspan=2, sticky="ew", padx=10, pady=10)
 top_frame.columnconfigure(0, weight=3)
 top_frame.columnconfigure(1, weight=1)
@@ -23,9 +24,9 @@ top_frame.columnconfigure(1, weight=1)
 left_buttons_frame = tb.Frame(top_frame)
 left_buttons_frame.grid(row=0, column=0, sticky="w")
 
-btn_left_1 = ttk.Button(left_buttons_frame, text="Dodaj notatkę", bootstyle="success-outline")
-btn_left_2 = ttk.Button(left_buttons_frame, text="Usuń notatkę", bootstyle="danger-outline")
-btn_left_3 = ttk.Button(left_buttons_frame, text="Edytuj notatkę", bootstyle="primary-outline")
+btn_left_1 = tb.Button(left_buttons_frame, text="Dodaj notatkę", bootstyle="success-outline")
+btn_left_2 = tb.Button(left_buttons_frame, text="Usuń notatkę", bootstyle="danger-outline")
+btn_left_3 = tb.Button(left_buttons_frame, text="Edytuj notatkę", bootstyle="primary-outline")
 btn_left_1.pack(side="left", padx=5)
 btn_left_2.pack(side="left", padx=5)
 btn_left_3.pack(side="left", padx=5)
@@ -33,8 +34,8 @@ btn_left_3.pack(side="left", padx=5)
 right_buttons_frame = tb.Frame(top_frame)
 right_buttons_frame.grid(row=0, column=1, sticky="e")
 
-btn_right_1 = ttk.Button(right_buttons_frame, text="Zmień tryb na jasny/ciemny", bootstyle="info-outline")
-btn_right_2 = ttk.Button(right_buttons_frame, text="Zintegruj z kalendarzem", bootstyle="primary-outline")
+btn_right_1 = tb.Button(right_buttons_frame, text="Zmień tryb na jasny/ciemny", bootstyle="info-outline")
+btn_right_2 = tb.Button(right_buttons_frame, text="Zintegruj z kalendarzem", bootstyle="primary-outline")
 
 btn_right_1.pack(side="top", fill="x", pady=5)
 btn_right_2.pack(side="top", fill="x", pady=5)
@@ -51,3 +52,8 @@ app.columnconfigure(1, weight=1)
 notes_frame.rowconfigure(0, weight=1)
 notes_frame.columnconfigure(0, weight=1)
 
+empty_label = tk.Label(notes_frame, text="BRAK NOTATEK",
+                 font=("Segoe UI", 30, "bold"),)
+empty_label.grid(row=0, column=0, sticky="nsew")
+
+app.mainloop()
